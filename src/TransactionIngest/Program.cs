7a1c052d1fd9c37033cc885cb82a33be09a21e2f
Akshith Reddy Musku <akshithreddymar@gmail.com>
2026-03-12 21:58:04 -0500

    adding intial solution structure and development setup

---
+++ src/TransactionIngest/Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Akshith Reddy Musku! How are you?");
---
+++ tests/TransactionIngest.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace TransactionIngest.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
